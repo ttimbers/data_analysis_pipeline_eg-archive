72f3b62d222cbd01747cbfd24fd136e2cab63314
timberst <tiffany.timbers@stat.ubc.ca>
2018-11-26 12:07:00 -0800

    fix to encoding problem

--- src/plotcount.py
+++ src/plotcount.py
@@ -9,7 +9,7 @@ parser.add_argument('output_file')
 args = parser.parse_args()
 
 def main():
-    data = pd.read_csv(args.input_file, sep = ' ', header = None)
+    data = pd.read_csv(args.input_file, sep = ' ', header = None, encoding = 'latin-1')
     data.rename(columns = {0:'word', 1:'count', 2:'frequency'}, inplace = True)
     data.sort_values(by = 'count')
     word_count_plot = data.iloc[:10,:2].plot.bar(x = 'word', y = 'count',
